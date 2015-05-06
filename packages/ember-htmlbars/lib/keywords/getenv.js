9d7c536f7d16888a283ceab7685bcf7d5fe28aa5
Edward Faulkner <ef@alum.mit.edu>
2015-05-06 19:41:15 -0400

    trying more stateful getenv

--- packages/ember-htmlbars/lib/keywords/getenv.js
+++ packages/ember-htmlbars/lib/keywords/getenv.js
@@ -5,11 +5,11 @@
 @submodule ember-htmlbars
 */
 
+import { internal } from "htmlbars-runtime";
+
 /**
   @method getenv
   @for Ember.Handlebars.helpers
   @param {String} property
 */
-export default function getEnvKeyword(morph, env, scope, params, hash, template, inverse) {
-  return env[env.hooks.getValue(params[0])];
-}
+export default ;
